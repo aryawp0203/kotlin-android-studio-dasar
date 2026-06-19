a78f8084876c256bcfc0500e3d3cdc4fb03ae146
aryawp0203 <labsi01pnc@gmail.com>
2026-06-19 15:28:58 +0700

    Mengenal Shared Preferences part-1

---
+++ MyAppLocal/app/src/main/java/id/my/labsi/myapplocal/EditorActivity.kt
@@ -0,0 +1,11 @@
+package id.my.labsi.myapplocal
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EditorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editor)
+    }
+}
